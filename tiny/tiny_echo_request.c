c5925063871c8a06d8ebb6053c6e419a20ef0812
jayjayppark <flhbigfamily7@gmail.com>
2024-04-21 15:46:47 +0900

    ii

--- tiny/tiny_echo_request.c
+++ tiny/tiny_echo_request.c
@@ -17,27 +17,32 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
-int main(int argc, char **argv) {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
-  /* Check command-line args */
-  if (argc != 2) {
-  fprintf(stderr, "usage: %s <port>\n", argv[0]);
-  exit(1);
-  }
-  listenfd = Open_listenfd(argv[1]);
-  while (1) {
-  clientlen = sizeof(clientaddr);
-  connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-  Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
-  port, MAXLINE, 0);
-  printf("Accepted connection from (%s, %s)\n", hostname, port);
-  doit(connfd);
-  Close(connfd);
-  }
-}
+void echo(int connfd);
+
+int main(int argc, char **argv)
+{
+int listenfd, connfd;
+socklen_t clientlen;
+struct sockaddr_storage clientaddr; /* Enough space for any address */
+ char client_hostname[MAXLINE], client_port[MAXLINE];
+
+ if (argc != 2) {
+ fprintf(stderr, "usage: %s <port>\n", argv[0]);
+ exit(0);
+ }
+
+ listenfd = Open_listenfd(argv[1]);
+ while (1) {
+ clientlen = sizeof(struct sockaddr_storage);
+ connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+ Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE,
+ client_port, MAXLINE, 0);
+ printf("Connected to (%s, %s)\n", client_hostname, client_port);
+ echo(connfd);
+ Close(connfd);
+ }
+ exit(0);
+ }
 
 void doit(int fd)
 {
@@ -84,6 +89,18 @@ void doit(int fd)
    serve_dynamic(fd, filename, cgiargs);
    }
 }
+
+void echo(int connfd)
+{
+size_t n;
+char buf[MAXLINE];
+rio_t rio;
+Rio_readinitb(&rio, connfd);
+ while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+ printf("server received %d bytes\n", (int)n);
+ Rio_writen(connfd, buf, n);
+ }
+ }
 
 void clienterror(int fd, char *cause, char *errnum,
 char *shortmsg, char *longmsg)
